b9b447c21061da5b8fa277945fb4f1cc68dfeeea
AkioOhtori <46541091+AkioOhtori@users.noreply.github.com>
2025-12-03 09:18:05 -0600

    Day 2 part 2

--- 2025/Day02/Day02.go
+++ 2025/Day02/Day02.go
@@ -17,14 +17,7 @@ func isError(err error) bool {
 	return (err != nil)
 }
 
-func main() {
-	//Open input file
-	var file, err = os.ReadFile(path)
-	if isError(err) {
-		return
-	}
-	input := strings.Split(string(file), ",")
-
+func part1(input []string) {
 	count := 0
 
 	for _, id := range input {
@@ -49,4 +42,53 @@ func main() {
 		}
 	}
 	fmt.Println("I think we've finished and the sum of all the invalid codes is:", count)
+}
+
+func part2(input []string) {
+	count := 0
+
+	for _, id := range input {
+		r := strings.Split(id, "-")
+
+		fin, _ := strconv.Atoi(r[1])
+		// Iterate over all of the possible IDs and see if they're symmetrical
+		for num, _ := strconv.Atoi(r[0]); num <= fin; num++ {
+			num_st := strconv.Itoa(num)
+
+		NumCheck:
+			for i := 1; i < len(num_st); i++ {
+				check := num_st[:i]
+				mult := len(num_st[i:]) % (i)
+				if mult != 0 {
+					continue
+				}
+				div := len(num_st[i:]) / i
+
+				for n := 0; n < div; n++ {
+					// monster := num_st[i+(n*i) : i+i+(n*i)]
+					if check == num_st[i+(n*i):i+i+(n*i)] {
+						if n+1 == div {
+							count += num
+							fmt.Println(num)
+							break NumCheck
+						}
+					} else {
+						break
+					}
+				}
+			}
+		}
+	}
+	fmt.Println("I think we've finished and the sum of all the invalid codes is:", count)
+}
+
+func main() {
+	//Open input file
+	var file, err = os.ReadFile(path)
+	if isError(err) {
+		return
+	}
+	input := strings.Split(string(file), ",")
+
+	part2(input)
 }
