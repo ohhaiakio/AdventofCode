840b51dc7b4c40877f8bda73348da31c0861703a
AkioOhtori <46541091+AkioOhtori@users.noreply.github.com>
2025-12-05 12:59:42 -0600

    Day 5 very broken part 2 do not use
    Still not getting the right answer, but wanting to tear this up so...

--- 2025/Day05/cafeteria.go
+++ 2025/Day05/cafeteria.go
@@ -8,7 +8,7 @@ import (
 	"strings"
 )
 
-var path = "sample.txt" //path to problem input
+var path = "input.txt" //path to problem input
 
 // Function to handle errors
 func isError(err error) bool {
@@ -21,11 +21,44 @@ func isError(err error) bool {
 func inRange(id int, fresh [][]int) bool {
 	for _, r := range fresh {
 		if id >= r[0] && id <= r[1] {
-			fmt.Println(id)
 			return true
 		}
 	}
 	return false
+}
+
+func inRange2(start int, finish int, fresh [][]int) []int {
+	for _, r := range fresh {
+		if start >= r[0] && start <= r[1] {
+			start = r[1] + 1
+		}
+		if finish >= r[0] && finish <= r[1] {
+			finish = r[0] - 1
+		}
+	}
+	if start > finish {
+		start = -99
+		finish = -99
+	}
+	return []int{start, finish}
+}
+
+func inRange3(id int, fresh [][]int) int {
+	c := 0
+	hits := []int{}
+	if id == -99 {
+		return 1
+	}
+	for i, r := range fresh {
+		if id >= r[0] && id <= r[1] {
+			hits = append(hits, i)
+			c++
+		}
+	}
+	// if c != 1 {
+	// 	fmt.Println(id, c, hits)
+	// }
+	return c
 }
 
 func main() {
@@ -44,6 +77,7 @@ func main() {
 
 	fresh := [][]int{}
 	preamble := true
+	part2 := 0
 
 	for scanner.Scan() {
 		// Grab instructions and save old data
@@ -56,7 +90,14 @@ func main() {
 			temp := strings.Split(txt, "-")
 			start, _ := strconv.Atoi(temp[0])
 			stop, _ := strconv.Atoi(temp[1])
-			fresh = append(fresh, []int{start, stop})
+			new_range := inRange2(start, stop, fresh)
+			new := new_range[1] - new_range[0] + 1
+			if new < 0 {
+				new = 0
+			} else {
+				fresh = append(fresh, new_range)
+			}
+
 		} else {
 			id, _ := strconv.Atoi(txt)
 			if inRange(id, fresh) {
@@ -66,7 +107,30 @@ func main() {
 	}
 	// And we're done!
 	fmt.Printf("\nThere were %v fresh ingredients in the first part.\n", count)
+	part2 = 0
+	for x := range fresh {
+		c := 0
+		c += inRange3(fresh[x][0], fresh)
+		c += inRange3(fresh[x][1], fresh)
+		new := 0
+		if c != 2 {
+			// fmt.Println("Found a problem!", x, c)
+			fresh[x] = inRange2(fresh[x][0], fresh[x][1], fresh)
+		}
+		new = fresh[x][1] - fresh[x][0] + 1
+		if new > 0 {
+			part2 += new
+		}
+	}
+	for x := range fresh {
+		c := 0
+		c += inRange3(fresh[x][0], fresh)
+		c += inRange3(fresh[x][1], fresh)
+		if c != 2 {
+			fmt.Println("Found a problem!", x, c)
+			// fresh[x] = inRange2(fresh[x][0], fresh[x][1], fresh)
+		}
+	}
 
-	// PART 2
-
+	fmt.Printf("\nThere are %v total fresh ingredients available.\n", part2)
 }
