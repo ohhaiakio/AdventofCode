6da99fefefcf1b17c9cdf56f2419ac91872a26bf
AkioOhtori <46541091+AkioOhtori@users.noreply.github.com>
2025-12-08 13:40:44 -0600

    Day 5 part 2 fail
    Not working for the time being

--- 2025/Day05/cafeteria.go
+++ 2025/Day05/cafeteria.go
@@ -28,7 +28,14 @@ func inRange(id int, fresh [][]int) bool {
 }
 
 func inRange2(start int, finish int, fresh [][]int) []int {
+	old_start := start
+	old_finish := finish
+
 	for _, r := range fresh {
+		if old_start == r[0] || old_finish == r[1] {
+			// fmt.Printf("Something weird happened. %v %v %v\n", r[0], r[1])
+			return []int{-999, -99}
+		}
 		if start >= r[0] && start <= r[1] {
 			start = r[1] + 1
 		}
@@ -37,7 +44,7 @@ func inRange2(start int, finish int, fresh [][]int) []int {
 		}
 	}
 	if start > finish {
-		start = -99
+		start = -999
 		finish = -99
 	}
 	return []int{start, finish}
@@ -45,19 +52,26 @@ func inRange2(start int, finish int, fresh [][]int) []int {
 
 func inRange3(id int, fresh [][]int) int {
 	c := 0
-	hits := []int{}
-	if id == -99 {
+	// hits := []int{}
+	if id == -99 || id == -999 {
 		return 1
 	}
-	for i, r := range fresh {
+	for _, r := range fresh {
 		if id >= r[0] && id <= r[1] {
-			hits = append(hits, i)
+			// hits = append(hits, i)
 			c++
 		}
 	}
 	// if c != 1 {
 	// 	fmt.Println(id, c, hits)
 	// }
+	return c
+}
+
+func detectRange(start int, stop int, fresh [][]int) int {
+	c := 0
+	c += inRange3(start, fresh)
+	c += inRange3(stop, fresh)
 	return c
 }
 
@@ -90,13 +104,7 @@ func main() {
 			temp := strings.Split(txt, "-")
 			start, _ := strconv.Atoi(temp[0])
 			stop, _ := strconv.Atoi(temp[1])
-			new_range := inRange2(start, stop, fresh)
-			new := new_range[1] - new_range[0] + 1
-			if new < 0 {
-				new = 0
-			} else {
-				fresh = append(fresh, new_range)
-			}
+			fresh = append(fresh, []int{start, stop})
 
 		} else {
 			id, _ := strconv.Atoi(txt)
@@ -107,28 +115,45 @@ func main() {
 	}
 	// And we're done!
 	fmt.Printf("\nThere were %v fresh ingredients in the first part.\n", count)
-	part2 = 0
+	hello_fresh := [][]int{}
 	for x := range fresh {
-		c := 0
-		c += inRange3(fresh[x][0], fresh)
-		c += inRange3(fresh[x][1], fresh)
-		new := 0
-		if c != 2 {
-			// fmt.Println("Found a problem!", x, c)
-			fresh[x] = inRange2(fresh[x][0], fresh[x][1], fresh)
+
+		hello_fresh = append(hello_fresh, inRange2(fresh[x][0], fresh[x][1], hello_fresh))
+
+		for detectRange(fresh[x][0], fresh[x][1], fresh) > 2 {
+			old := fresh[x]
+			fresh[x] = []int{-777, -777}
+			// fresh[x] = inRange2(fresh[x][0], fresh[x][1], fresh)
+			fresh[x] = inRange2(old[0], old[1], fresh)
 		}
-		new = fresh[x][1] - fresh[x][0] + 1
-		if new > 0 {
-			part2 += new
+
+		if fresh[x][1] > 0 {
+			calc := fresh[x][1] - fresh[x][0] + 1
+			if calc > 0 {
+				part2 += calc
+			}
+		}
+
+		// fmt.Printf("\nStart: %v\tFinish: %v\tCalc: %v\n", fresh[x][0], fresh[x][1], calc)
+
+	}
+
+	for x := range fresh {
+		if fresh[x][1] > 0 {
+			calc := fresh[x][1] - fresh[x][0] + 1
+			if calc > 0 {
+				part2 += calc
+			}
 		}
 	}
-	for x := range fresh {
-		c := 0
-		c += inRange3(fresh[x][0], fresh)
-		c += inRange3(fresh[x][1], fresh)
-		if c != 2 {
-			fmt.Println("Found a problem!", x, c)
-			// fresh[x] = inRange2(fresh[x][0], fresh[x][1], fresh)
+	fmt.Printf("\nThere are %v total fresh ingredients available.\n", part2)
+	part2 = 0
+	for x := range hello_fresh {
+		if hello_fresh[x][1] > 0 {
+			calc := hello_fresh[x][1] - hello_fresh[x][0] + 1
+			if calc > 0 {
+				part2 += calc
+			}
 		}
 	}
 
